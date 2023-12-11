5d2b04f930ad03906b225ba3eef63ad44cd0d910
cheng-hsiang-chiu <chenghsiang.chiu@gmail.com>
2023-12-11 14:16:15 -0600

    updated

--- README.md
+++ README.md
@@ -6,16 +6,19 @@ It is to study the runtime performance of DOCA Graph programming model.
 
 
 ## Repository structure
-After unzip the final_project.zip, there are five files
-- graph_main.c: main function in the DOCA Graph implementation
-- graph_sample.c: function definitions in the DOCA Graph implementation
-- pthread_sample.c: a Pthread implementation
-- sequential_sample.c: a sequential implementation
-- run.sh: a script to compile and run the experiment
+There are several files in the repository. They are:
+- run.sh : a script to compile and run the experiment
+- src : source code
+  - graph_main.c: main function in the DOCA Graph implementation
+  - graph_sample.c: function definitions in the DOCA Graph implementation
+  - pthread_sample.c: a Pthread implementation
+  - sequential_sample.c: a sequential implementation
+- SmartNIC___Final_Project.pd : the report
+- smartnic.pdf : the presentation slide
 
 ## Build and Run
 To build and run the three implementations,
-***You need to login in the DPU first and unzip the file in /opt/mellanox/doca/samples/doca_common***
+***You need to login in the DPU first and unzip the file in /opt/mellanox/doca/samples/doca_common. You might be sudo for the operation.***
 Then simply type the command:
 ```
 ./run.sh
--- run.sh
+++ run.sh
@@ -3,15 +3,15 @@
 echo "Start to compile DOCA Graph code"
 echo ""
 
-sudo gcc -L ../../../lib/aarch64-linux-gnu/ -Wno-missing-braces -D DOCA_ALLOW_EXPERIMENTAL_API graph_main.c graph_sample.c ../../common.c ../../../applications/common/src/utils.c -I../../../applications/common/src/ -I../ -I../../ -I ../../../ -I../../../include/ -ldoca_common -ldoca_dma -ldoca_sha -o graph_sample
+sudo gcc -L ../../../lib/aarch64-linux-gnu/ -Wno-missing-braces -D DOCA_ALLOW_EXPERIMENTAL_API ./src/graph_main.c ./src/graph_sample.c ../../common.c ../../../applications/common/src/utils.c -I../../../applications/common/src/ -I../ -I../../ -I ../../../ -I../../../include/ -ldoca_common -ldoca_dma -ldoca_sha -o graph_sample
 
 echo "Start to compile Pthread code"
 echo ""
-sudo gcc -L ../../../lib/aarch64-linux-gnu/ -Wno-missing-braces -D DOCA_ALLOW_EXPERIMENTAL_API pthread_sample.c ../../common.c ../../../applications/common/src/utils.c -I../../../applications/common/src/ -I../ -I../../ -I ../../../ -I../../../include/ -ldoca_common -ldoca_dma -ldoca_sha -o pthread_sample
+sudo gcc -L ../../../lib/aarch64-linux-gnu/ -Wno-missing-braces -D DOCA_ALLOW_EXPERIMENTAL_API ./src/pthread_sample.c ../../common.c ../../../applications/common/src/utils.c -I../../../applications/common/src/ -I../ -I../../ -I ../../../ -I../../../include/ -ldoca_common -ldoca_dma -ldoca_sha -o pthread_sample
 
 echo "Start to compile sequential code"
 echo ""
-sudo gcc -L ../../../lib/aarch64-linux-gnu/ -Wno-missing-braces -D DOCA_ALLOW_EXPERIMENTAL_API sequential_sample.c ../../common.c ../../../applications/common/src/utils.c -I../../../applications/common/src/ -I../ -I../../ -I ../../../ -I../../../include/ -ldoca_common -ldoca_dma -ldoca_sha -o sequential_sample
+sudo gcc -L ../../../lib/aarch64-linux-gnu/ -Wno-missing-braces -D DOCA_ALLOW_EXPERIMENTAL_API ./src/sequential_sample.c ../../common.c ../../../applications/common/src/utils.c -I../../../applications/common/src/ -I../ -I../../ -I ../../../ -I../../../include/ -ldoca_common -ldoca_dma -ldoca_sha -o sequential_sample
 
 echo "Finish compiling"
 
@@ -52,3 +52,7 @@ done
 
 echo "------------------------------ Finish running sequential implementation -----------------------------"
 echo ""
+
+sudo rm graph_sample
+sudo rm pthread_sample
+sudo rm sequential_sample
